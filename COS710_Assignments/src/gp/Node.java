f6746939089185c515a4f588f454f22fc3ceae23
Alexander-Muendesi <u20426918@tuks.co.za>
2023-03-01 00:41:12 +0200

    ✨ Added a depth attribute and a getValue method

--- COS710_Assignments/src/gp/Node.java
+++ COS710_Assignments/src/gp/Node.java
@@ -4,6 +4,7 @@ package gp;
  * @brief This class acts an abstract parent to cater for the FunctionNode and TerminalNode classes.
  */
 public abstract class Node {
+    public int depth = -1;//temporary for now, used for printing purposes
     /**
      * @brief The evaluate function has 2 meaning based on whether we are using a Terminal or Function
             node. With a terminal node it will just return the value of the terminal. With a Function node 
@@ -16,4 +17,5 @@ public abstract class Node {
      * @throws Exception
      */
     public abstract double evaluate(double[] inputData) throws Exception;
+    public abstract String getValue() throws Exception;
 }
